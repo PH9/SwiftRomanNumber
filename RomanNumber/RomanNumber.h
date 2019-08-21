e7838d82786025d1856c2e7c0c450a2205f2bfe0
Wasith Theerapattrathamrong <phai@live.com>
2019-08-21 14:19:46 +0700

    Project init

---
+++ RomanNumber/RomanNumber.h
@@ -0,0 +1,19 @@
+//
+//  RomanNumber.h
+//  RomanNumber
+//
+//  Created by Wasith Theerapattrathamrong on 21/8/2562 BE.
+//  Copyright © 2562 Wasith Theerapattrathamrong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RomanNumber.
+FOUNDATION_EXPORT double RomanNumberVersionNumber;
+
+//! Project version string for RomanNumber.
+FOUNDATION_EXPORT const unsigned char RomanNumberVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RomanNumber/PublicHeader.h>
+
+
