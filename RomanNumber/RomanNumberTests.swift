0436636b6860b19582d6cbbfb139a9eb29f4c569
Wasith Theerapattrathamrong <phai@live.com>
2019-08-21 15:25:02 +0700

    Combine test functions

--- RomanNumber/RomanNumberTests.swift
+++ RomanNumber/RomanNumberTests.swift
@@ -3,33 +3,15 @@ import XCTest
 
 class RomanNumberTests: XCTestCase {
 
-  func testRomanNumber_1_to_3() {
+  func testRomanNumber_1_to_9() {
     XCTAssertEqual("I", 1.roman)
     XCTAssertEqual("II", 2.roman)
     XCTAssertEqual("III", 3.roman)
-  }
-
-  func testRomanNumber_4_is_IV() {
     XCTAssertEqual("IV", 4.roman)
-  }
-
-  func testRomanNumber_5_is_V() {
     XCTAssertEqual("V", 5.roman)
-  }
-
-  func testRomanNumber_6_is_VI() {
     XCTAssertEqual("VI", 6.roman)
-  }
-
-  func testRomanNumber_7_is_VII() {
     XCTAssertEqual("VII", 7.roman)
-  }
-
-  func testRomanNumber_8_is_VIII() {
     XCTAssertEqual("VIII", 8.roman)
-  }
-
-  func testRomanNumber_9_is_IX() {
     XCTAssertEqual("IX", 9.roman)
   }
 }
